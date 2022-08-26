ee18ee016e9bf069365dd5a4afd3aa6af9952b0d
lenovo <lenovo@Swapnil>
2022-08-26 18:09:49 +0530

    Swapnil- add opelUrl2 file pushed

---
+++ Proj_TestNG/src/WebDriver/openUrl2.java
@@ -0,0 +1,20 @@
+package WebDriver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class openUrl2 {
+	@Test
+	public void f() {
+		
+		System.setProperty("webdriver.chrome.driver", "E:/Driver/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		// open current url in browser
+		driver.get("https://google.co.in");
+		
+		// alternative method to open url in browser
+		driver.navigate().to("https://w3schools.com");
+	}
+}
