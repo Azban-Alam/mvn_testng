6631ae853c9d66462dc362411cad1257cbac7ba0
user <user@DESKTOP-SITNGUM>
2022-08-27 11:32:19 +0530

    Israr Alam : Study Stashe Topic

---
+++ Proj_TestNG/src/StudyPckage/NewTest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package StudyPckage;
+
+/**
+ * @author user
+ *
+ */
+public class NewTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
